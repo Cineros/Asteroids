d46cf55eb04e5acbcb154c067297a7c58ac99c93
Darius Wolfe <Darius.Wolfe@live.com>
2024-12-29 19:21:39 -0500

    Changed gitignore and added delta time

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock
+    dt = 0
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -12,6 +14,8 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        game_clock.tick(60)
+        dt = game_clock.tick(60)/1000
     
 
 if __name__ == "__main__":
