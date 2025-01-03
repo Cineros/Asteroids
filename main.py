9d046751f502ddeb47d00b8508340d0f1063bffc
Darius Wolfe <Darius.Wolfe@live.com>
2025-01-03 16:52:57 -0500

    Utilized delta to add turning to player.

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import pygame
 from constants import *
+from player import *
 def main():
     pygame.init()
-    game_clock = pygame.time.Clock
-    dt = 0
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     print("Starting asteroids!")
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0,0,0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-        game_clock.tick(60)
-        dt = game_clock.tick(60)/1000
+
+        dt = game_clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
